c7c73a9f16a4ba61ccf113819fd5fac5d3263d2e
TommasoPappagallo <pappagallot@gmail.com>
2022-10-20 15:56:34 +0100

    fixed my calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,23 +1,5 @@
 #include "libft.h"
 
-/* void	*ft_calloc(size_t count, size_t size)
-{
-	char	*p;
-	size_t 	cnt;
-	p = (char *)malloc(count * size);
-	if(p==NULL)
-		return NULL;
-	cnt = 0;
-
-	while (cnt < count * size){
-		*(p+cnt++) = 0;
-
-	}
-
-		//ft_memset(p, 0, count * size);
-
-	return ((void *)p);
-} */
 void	*ft_calloc(size_t n_ele, size_t size_ele)
 {
 	char	*dyn_ptr;
